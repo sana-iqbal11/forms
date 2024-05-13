02c400dfc2355178104f9e60a91d21466bc554ad
sana-iqbal11 <139572147+sana-iqbal11@users.noreply.github.com>
2024-05-13 23:53:28 +0500

    hsgahgsaj

--- pages/contactus/index.js
+++ pages/contactus/index.js
@@ -17,28 +17,26 @@ function ContactForm() {
   // };
   
   return (
-    <div>
-    <div>
-    <form name="contact" method="post" netlify-honeypot="bot-field">
-      <p>
-        <label htmlFor="name">Name</label> <br />
-        <input type="text" id="name" name="name" required />
-      </p>
-      <p>
-        <label htmlFor="email">Email</label> <br />
-        <input type="email" id="email" name="email" required />
-      </p>
-      <p>
-        <label htmlFor="message">Message</label> <br />
-        <textarea id="message" name="message" required></textarea>
-      </p>
-      <p>
-        <input type="submit" value="Submit message" />
-      </p>
-    </form>
+    <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
 
-    </div>
-  </div>
+    <p>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" />
+    </p>
+    <p>
+      <label htmlFor="email">Email</label>
+      <input type="text" id="email" name="email" />
+    </p>
+    <p>
+      <label htmlFor="message">Message</label>
+      <textarea id="message" name="message"></textarea>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+
   );
 }
 
