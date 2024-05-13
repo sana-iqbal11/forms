037233f6050570b09ddb160b4ec5675987d38865
sana-iqbal11 <139572147+sana-iqbal11@users.noreply.github.com>
2024-05-13 23:25:41 +0500

    hsgs

--- pages/contactus/index.js
+++ pages/contactus/index.js
@@ -1,65 +1,41 @@
-import { useState } from 'react';
+import React, { Component } from 'react'
 
-const VoucherOrderForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [value, setValue] = useState('');
-  const [personName, setPersonName] = useState('');
-  const [personPhone, setPersonPhone] = useState('');
+import { NetlifyForm, Honeypot } from 'react-netlify-forms'
 
-  const [agreed, setAgreed] = useState<boolean>(false);
+const ContactForm = () => {
+    return(
 
-  const handleClick = () => {
-    setAgreed(!agreed);
-  };
+    <NetlifyForm name='Contact' action='/thanks' honeypotName='bot-field'>
+    {({ handleChange, success, error }) => (
+      <>
+        <Honeypot />
+        {success && <p>Thanks for contacting us!</p>}
+        {error && (
+          <p>Sorry, we could not reach our servers. Please try again later.</p>
+        )}
+        <div>
+        <input type="hidden" name="form-name" value="Contact"/>
 
-  const isValid = () => agreed && name && email && value && personName && personPhone;
+          <label htmlFor='name'>Name:</label>
+          <input type='text' name='name' id='name' onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='message'>Message:</label>
+          <textarea
+            type='text'
+            name='message'
+            id='message'
+            rows='4'
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type='submit'>Submit</button>
+        </div>
+      </>
+    )}
+  </NetlifyForm>
+    )
+}
 
-  return (
-    <form name="voucher" data-netlify="true" className={styles.courseForm} method="POST" action="/poukaz-objednan" netlify-honeypot="bot-field">
-      <h3>Objednávka voucheru</h3>
-      <p className="hidden">
-        <label>
-          Do not fill this out if you are a human: <input name="bot-field" />
-        </label>
-      </p>
-      <input type="hidden" name="form-name" value="voucher" />
-      <div >
-        <label htmlFor="voucher_jmeno">Celé jméno</label>
-        <input type="text" id="voucher_jmeno" name="jmeno" onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div >
-        <label htmlFor="voucher_email">Email</label>
-        <input type="email" id="voucher_email" name="email" onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div >
-        <label>Typ poukazu</label>Dentální hygiena
-      </div>
-      <div >
-        <label htmlFor="voucher_hodnota">Hodnota</label>
-        <input type="text" id="voucher_hodnota" name="hodnota" onChange={(e) => setValue(e.target.value)} />
-      </div>
-      <div >
-        <label htmlFor="voucher_prijemce">Komu poukaz daruji</label>
-        <input type="text" id="voucher_prijemce" name="prijemce" onChange={(e) => setPersonName(e.target.value)} />
-      </div>
-      <div >
-        <label htmlFor="voucher_tel">Telefon obdarovaného</label>
-        <input type="phone" id="voucher_tel" name="telefon" onChange={(e) => setPersonPhone(e.target.value)} />
-      </div>
-
-      <div >
-        <input type="checkbox" name="souhlas" id="souhlas" onClick={handleClick} />
-        <label htmlFor="souhlas">Souhlasím se zpracováním všech údajů</label>
-      </div>
-
-      <div >
-        <button type="submit" role="button" disabled={!isValid()}>
-          Objednat
-        </button>
-      </div>
-    </form>
-  );
-};
-
-export default VoucherOrderForm;
+export default ContactForm
