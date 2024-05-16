7e8d0a838827ae2afa6204a08a0667e98c811fb3
sana-iqbal11 <139572147+sana-iqbal11@users.noreply.github.com>
2024-05-16 14:42:59 +0500

    chnges

--- pages/contactus/index.js
+++ pages/contactus/index.js
@@ -21,15 +21,15 @@ function ContactForm() {
         <input type="hidden" name="form-name" value="contact" />
 
     <p>
-      <label htmlFor="name">Name</label>
+      {/* <label htmlFor="name">Name</label> */}
       <input type="text" id="name" name="name" />
     </p>
     <p>
-      <label htmlFor="email">Email</label>
+      {/* <label htmlFor="email">Email</label> */}
       <input type="text" id="email" name="email" />
     </p>
     <p>
-      <label htmlFor="message">Message</label>
+      {/* <label htmlFor="message">Message</label> */}
       <textarea id="message" name="message"></textarea>
     </p>
     <p>
