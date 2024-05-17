78287456b8ca02d682a49c7834490e488d10f419
sana-iqbal11 <139572147+sana-iqbal11@users.noreply.github.com>
2024-05-17 19:33:42 +0500

    chnes

--- pages/contactus/index.js
+++ pages/contactus/index.js
@@ -1,20 +1,9 @@
 import React from "react";
-import SectionTitle from "./sectionTitle";
-import ColoredSpan from "./coloredSpan";
-import HeadingDescription from "./headingDescription";
 
 function ContactForm() {
   return (
     <div className="grid grid-cols-1 gap-5 place-content-center h-full lg:px-16 px-3">
-      <SectionTitle className="lg:text-start text-center">
-        <ColoredSpan className="text-white laptop:text-5xl text-3xl">
-          Let’s level up your brand, together
-        </ColoredSpan>
-      </SectionTitle>
-      <HeadingDescription className="text-[16px] lg:text-start text-center mt-[-28px]">
-        You can reach us anytime via{" "}
-        <span className="text-orange">info@e3.ventures</span>
-      </HeadingDescription>
+    
       
             <form className="flex flex-col gap-4" name="contact" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
